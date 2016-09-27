43b1d7bf15bf01aa0d5f1b7a52661854dfce2118
jenia996 <jenian996@gmail.com>
2016-09-28 00:28:30 +0300

    Binded textView.

--- MyApplication/app/src/main/java/com/example/ajax/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/ajax/myapplication/MainActivity.java
@@ -8,8 +8,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        textView = (TextView) findViewById(R.id.hello_world);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
