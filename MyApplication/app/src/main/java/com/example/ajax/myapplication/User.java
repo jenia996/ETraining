0816119701e751becd865a866c6dddbfaad84736
jenia996 <jenian996@gmail.com>
2016-09-28 00:23:08 +0300

    Create User class
    Change toolbar title

---
+++ MyApplication/app/src/main/java/com/example/ajax/myapplication/User.java
@@ -0,0 +1,18 @@
+package com.example.ajax.myapplication;
+
+/**
+ * Created by Ajax on 28.09.2016.
+ */
+
+public class User {
+    private String id;
+    private String userName;
+
+    public User(String id, String userName) {
+        this.id = id;
+        this.userName = userName;
+    }
+
+    public User() {
+    }
+}
